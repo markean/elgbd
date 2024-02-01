2c95728dda155ed4f6c8b060816c995e38716389
markean <markean@protonmail.com>
2024-02-01 17:52:21 -0500

    initiate submission proj

--- R/elgbd-package.R
+++ R/elgbd-package.R
@@ -1,3 +1,10 @@
+#' @references Kim E, MacEachern SN, Peruggia M (2023).
+#'   "Empirical likelihood for the analysis of experimental designs."
+#'   \emph{Journal of Nonparametric Statistics}, **35**(4), 709--732.
+#'   \doi{10.1080/10485252.2023.2206919}.
+#' @keywords internal
+"_PACKAGE"
+
 ## usethis namespace: start
 #' @useDynLib elgbd, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
